b449d1f1bb421c2a8a18f6b95ee77039681c9ccf
Edwin Fom <edwinfom05@gmail.com>
2024-01-14 03:16:29 +0100

    modified

--- app/src/main/java/com/example/mysecondapp/OnBoardingActivity.java
+++ app/src/main/java/com/example/mysecondapp/OnBoardingActivity.java
@@ -1,14 +1,30 @@
 package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SlideAdapter slideAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+
+        //call Slider Adapter Method
+
+        slideAdapter = new SlideAdapter(this);
+
+        viewPager.setAdapter(slideAdapter);
     }
 }
